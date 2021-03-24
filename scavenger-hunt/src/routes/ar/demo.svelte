6973b124e88cef9fbbd3508449cd8be21377b5a8
jgero <33359660+jgero@users.noreply.github.com>
2021-03-24 10:49:54 +0100

    vr init for deploy test

--- scavenger-hunt/src/routes/ar/demo.svelte
+++ scavenger-hunt/src/routes/ar/demo.svelte
@@ -1,11 +1,21 @@
 <script>
-	console.log("I am alive!");
+	import { onMount } from "svelte";
+	onMount(() => {
+			console.log("I am alive!");
+			// 48.937930742030865, 10.209591656625395
+		setInterval(() => {
+			const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+			console.log("measuring: ");
+			console.log(distanceMsg);
+		}, 3000);
+	});
+	// <a-box material="color: yellow" gps-entity-place="latitude: <your-latitude>; longitude: <your-longitude>"/>
 </script>
 
-<svelte:head>
-	<script src="https://aframe.io/releases/1.2.0/aframe.min.js">
-	</script>
-	<script
-		src="https://raw.githack.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.min.js">
-	</script>
-</svelte:head>
+<a-scene
+	vr-mode-ui="enabled: true"
+	embedded
+	arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: true;">
+	<a-camera gps-camera rotation-reader />
+	<a-box material="color: yellow"  gps-entity-place="latitude: 48.938411863997246; longitude: 10.209873807386952"/>
+</a-scene>
