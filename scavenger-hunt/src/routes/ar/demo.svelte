273cdaddde9be7bfdf5755f8423529610c150ed9
jgero <33359660+jgero@users.noreply.github.com>
2021-03-24 09:21:31 +0100

    scavenger-hunt page init

---
+++ scavenger-hunt/src/routes/ar/demo.svelte
@@ -0,0 +1,11 @@
+<script>
+	console.log("I am alive!");
+</script>
+
+<svelte:head>
+	<script src="https://aframe.io/releases/1.2.0/aframe.min.js">
+	</script>
+	<script
+		src="https://raw.githack.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.min.js">
+	</script>
+</svelte:head>
