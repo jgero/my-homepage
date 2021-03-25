3899641f545660a42faf7bb52bf02a23729e7bf9
jgero <33359660+jgero@users.noreply.github.com>
2021-03-25 17:05:01 +0100

    hello world with custom logger

---
+++ scavenger-hunt/src/components/Logger.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { getLogger } from "../stores/debug-logger";
+	const logger = getLogger();
+</script>
+
+<section>
+	{#each $logger as item}<span>{item.message}</span> <br />{/each}
+</section>
