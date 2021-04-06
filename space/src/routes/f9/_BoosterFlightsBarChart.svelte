4ffc4cd1a26c049de23b9f8560274419df212274
jgero <33359660+jgero@users.noreply.github.com>
2021-04-06 15:25:10 +0200

    more interesting booster flight chart

--- space/src/routes/f9/_BoosterFlightsBarChart.svelte
+++ space/src/routes/f9/_BoosterFlightsBarChart.svelte
@@ -1,33 +1,182 @@
 <script>
-  import { onMount } from "svelte";
-  import BarChart from "../../components/BarChart.svelte";
+  import { onMount } from 'svelte';
 
-  let hasLoaded = false;
-  let data = {
-    xAxis: "booster id",
-    yAxis: "flights",
-    data: [],
-    barColor: "#f1c46d",
-    averageLineColor: "#e6e6e6",
-  };
+  // all data about the cores and the launches
+  let baseData;
+  // how big is the percentage of starlink missions of the reused boosters
+  let starlinkPercentage;
+  const scale = 60;
 
   onMount(() => {
     // get all cores from the api
-    fetch("https://api.spacexdata.com/v4/cores?block=5")
-      // just keep the json part of it
-      .then((response) => response.json())
-      // render the chart
-      .then((responseJSON) => {
-        data.data = responseJSON
-          .filter((core) => core.block >= 5)
-          .map((core) => core.reuse_count + 1);
-        // data is now completely loaded
-        hasLoaded = true;
+
+    baseData = Promise.all([
+      fetch('https://api.spacexdata.com/v4/cores/query', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: { block: 5 },
+          options: { limit: 100 },
+        }),
+      })
+        // just keep the json part of it
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          responseJSON = responseJSON.docs.map((core) => ({
+            flights: core.launches.length,
+            isLost: core.status === 'expended' || core.status === 'lost',
+            id: core.id,
+          }));
+          return responseJSON;
+        }),
+      // get all f9 starlink launches
+      fetch('https://api.spacexdata.com/v4/launches/query', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: { $text: { $search: 'starlink' } },
+          options: { limit: 100 },
+        }),
+      })
+        // just keep the json part of it
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          // only keep launches with one core
+          responseJSON = responseJSON.docs.filter(
+            (launch) => launch.cores.length === 1
+          );
+          responseJSON = responseJSON.map((launch) => launch.cores[0].core);
+          return responseJSON;
+        }),
+    ]).then(([cores, starlinkLaunches]) => {
+      const coreMap = new Map();
+      cores.forEach(({ id, flights, isLost }) => {
+        coreMap.set(id, { flights, isLost, starlinkLaunches: 0 });
       });
+      starlinkLaunches.forEach((coreId) => {
+        if (!coreMap.has(coreId)) {
+          console.log('core map has not core with that id');
+          return;
+        }
+        const { id, flights, isLost, starlinkLaunches } = coreMap.get(coreId);
+        coreMap.set(coreId, {
+          id,
+          flights,
+          isLost,
+          starlinkLaunches: starlinkLaunches + 1,
+        });
+      });
+      return Array.from(coreMap)
+        .map(([id, data]) => ({ id, ...data }))
+        .map((core) => {
+          const { starlinkLaunches, flights } = core;
+          // do nothing when no flights yet happeded
+          if (flights === 0) {
+            return core;
+          }
+          return {
+            ...core,
+            starlinkLaunches:
+              Math.round((starlinkLaunches / flights) * 100) / 100,
+          };
+        });
+    });
+
+    starlinkPercentage = baseData.then((cores) => {
+      const test = new Map();
+      cores
+        .filter((c) => c.flights > 1)
+        .forEach((core) => {
+          if (test.has(core.starlinkLaunches)) {
+            test.set(
+              core.starlinkLaunches,
+              test.get(core.starlinkLaunches) + core.flights
+            );
+          } else {
+            test.set(core.starlinkLaunches, core.flights);
+          }
+        });
+      console.log(cores.filter((c) => c.flights > 1));
+      return cores;
+    });
   });
 </script>
 
+<figure>
+  {#await baseData}
+    <p>loading...</p>
+  {:then cores}
+    <svg viewBox="0 0 1400 700">
+      <g class="legend">
+        <g class="labels">
+          <text x={700 - 8 * scale - 10} y="700" text-anchor="end"
+            >100% Starlink</text
+          >
+          <text x={700 + 8 * scale + 10} y="700" text-anchor="start"
+            >0% Starlink</text
+          >
+          <text text-anchor="middle" x="700" y={700 - 1 * scale - 2}
+            >1 Flight</text
+          >
+          <text text-anchor="middle" x="700" y={700 - 4 * scale - 2}
+            >4 Flights</text
+          >
+          <text text-anchor="middle" x="700" y={700 - 8 * scale - 2}
+            >8 Flights</text
+          >
+        </g>
+        <circle cx="700" cy="700" r={8 * scale} />
+        <circle cx="700" cy="700" r={4 * scale} />
+        <circle cx="700" cy="700" r={1 * scale} />
+      </g>
+      <g class="cores">
+        {#if cores}
+          {#each cores as core}
+            <rect
+              x="700"
+              y="685"
+              rx="5"
+              ry="5"
+              width={core.flights * scale}
+              height="30"
+              transform="rotate({core.starlinkLaunches * -180} 700 700)"
+            />
+          {/each}
+        {/if}
+      </g>
+    </svg>
+  {/await}
+  <figcaption>
+    <h3>booster flights</h3>
+    <p>
+      The diagram shows the amount of flights of the boosters that have flown
+      since 2014 sorted by date of the first flight
+    </p>
+  </figcaption>
+</figure>
+
 <style>
+  svg g.legend circle {
+    fill: transparent;
+    stroke: #e6e6e6;
+  }
+  svg g.legend text {
+    fill: #e6e6e6;
+  }
+  svg g.cores rect {
+    fill: #f1c46d;
+    stroke: #e6e6e6;
+    transition: all 0.3s ease-in-out;
+  }
+  svg g.cores rect:hover {
+    fill: red;
+  }
   figure {
     display: flex;
     align-items: center;
@@ -57,18 +206,3 @@
     color: #f1c46d;
   }
 </style>
-
-<figure>
-  {#if !hasLoaded}
-    <p>loading...</p>
-  {:else}
-    <BarChart {data} />
-  {/if}
-  <figcaption>
-    <h3>booster flights</h3>
-    <p>
-      The diagram shows the amount of flights of the boosters that have flown
-      since 2014 sorted by date of the first flight
-    </p>
-  </figcaption>
-</figure>
