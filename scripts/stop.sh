5d48328f50a6374c9465755bfca0664d1b885c3c
jgero <33359660+jgero@users.noreply.github.com>
2021-06-04 15:00:39 +0200

    updated stop script

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# try to stop running dev pod
-RUNNING_DEV_POD_COUNT=$(podman pod ls | grep " my-webpage-dev " | wc -l)
+# try to stop running pod
+RUNNING_POD_COUNT=$(podman pod ls | grep " my-webpage-dev " | wc -l)
 
-if [ "$RUNNING_DEV_POD_COUNT" -gt 0 ]; then
+if [ "$RUNNING_POD_COUNT" -gt 0 ]; then
 	echo "==> stopping pod"
 	podman pod stop my-webpage-dev
 	echo "==> [DONE]"
